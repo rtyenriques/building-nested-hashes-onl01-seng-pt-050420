4a6bc21cbef34413e65e4e8aae15c986ba9ae299
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-14 20:37:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,6 +22,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name:"",
+        age:""
         },
       :heroine => {
         #your key/value pairs here
