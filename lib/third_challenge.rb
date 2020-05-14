78838cb77fab776320d31feadd429b9acb136486
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-14 20:42:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,8 +23,8 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        name:"",
-        age:""
+        name:"Lord Capulet",
+        age:"50"
         },
       :matriarch => {
 
